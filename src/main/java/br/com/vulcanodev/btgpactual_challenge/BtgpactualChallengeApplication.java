0c3855a90853c2c61bb395df8fa04f5599f38395
DouglasVulcano <vulcanodouglas1708@gmail.com>
2025-07-18 07:43:44 -0300

    first commit

---
+++ src/main/java/br/com/vulcanodev/btgpactual_challenge/BtgpactualChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.vulcanodev.btgpactual_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtgpactualChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtgpactualChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vulcanodev/btgpactual_challenge/BtgpactualChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vulcanodev.btgpactual_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtgpactualChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
